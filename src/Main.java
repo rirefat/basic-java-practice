a45bdcd76d76e88fb1b9ac1493c654626cfe3c25
rirefat <rirefat.official@gmail.com>
2023-04-27 17:42:01 +0600

    multi dimensional array

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,15 @@ public class Main {
         String name = "Md. Rafiul Islam Refat";
         System.out.println(name.replace("Md.", ""));
 
+        // One dimensional array
         int[] num = {12,5,9,3};  // Declare array with elements
         Arrays.sort(num);  // Sorting array elements
-        System.out.println(Arrays.toString(num));  // Printing array
+        // Printing array (use toString for one dimensional array)
+        System.out.println(Arrays.toString(num));
+
+        // Multi-dimensional array
+        int[][] numSet2 = { {1,2,3}, {4,5,6} };  // Declare two-dimensional array
+        // Printing two-dimensional array. deepToString() is used to print multidimensional array
+        System.out.println(Arrays.deepToString(numSet2));
     }
 }
